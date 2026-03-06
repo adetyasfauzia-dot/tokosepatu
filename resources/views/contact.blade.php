937fc482c57462c8c6a040b1e804ecea9a077a72
Adetyas Fauzia <adetyasfauzia@gmail.com>
2026-03-07 02:18:34 +0700

    menambah file README dan test23

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,4 @@
+<h1>Halaman Contact</h1>
+<p>Hubungi kami untuk informasi penyewaan baju.</p>
+<p>Email: tokosewabaju@gmail.com</p>
+<a href="/">Kembali ke Home</a>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <!-- Simplicity is an acquiredd taste. - Katharine Gerould -->
+Selamat Datang di Toko Sewa Baju Terbaik {{$nama}}<br>
+Anda adalah seorang {{$pekerjaan}}
+</div>
